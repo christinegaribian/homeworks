3a2ab5440d08a4c1cf5fad2cb18e4697f47bfd28
Christine <christinegaribian@gmail.com>
2017-04-13 09:11:24 -0700

    Complete Movie Buff in Training

---
+++ W3/W3D3/Neighborhood/db/migrate/20170412165623_create_people.rb
@@ -0,0 +1,10 @@
+class CreatePeople < ActiveRecord::Migration[5.0]
+  def change
+    create_table :people do |t|
+      t.integer :house_id
+      t.string :name
+      
+      t.timestamps
+    end
+  end
+end
---
+++ W3/W3D4/skeleton/app/models/actor.rb
@@ -0,0 +1,9 @@
+require 'byebug'
+
+class Actor < ActiveRecord::Base
+	has_many :castings
+	has_many :movies, through: :castings
+	has_many :directed_movies,
+		foreign_key: :director_id,
+		class_name: "Movie"
+end
