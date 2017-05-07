6109ed3402f1b2d056367c46e3dd0a1cb7fdd360
Christine <christinegaribian@gmail.com>
2017-05-07 14:27:41 -0700

    Redux homework

---
+++ W7/W7D1/skeleton/frontend/actions.js
@@ -0,0 +1,8 @@
+// action creator
+const selectCurrency = (baseCurrency, rates) =>({
+  type: "SWITCH_CURRENCY",
+  baseCurrency: baseCurrency,
+  rates: rates
+});
+
+export default selectCurrency;
